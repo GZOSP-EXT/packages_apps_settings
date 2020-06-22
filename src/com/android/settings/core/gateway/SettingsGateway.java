adindotdev/android_packages_apps_Settings

Settings: External Smart Pixels fragment launch
Necessary for Smart Pixels tile.
Based on change by Mazda--.

Reference commit:
DirtyUnicorns@c637500b89bfe1b726a92efa358cf48b1a2a3093

Change-Id: I5a1892d8211ac968958395326fbbbfcdfcf7e942

src/com/android/settings/core/gateway/SettingsGateway.java
@@ -138,6 +138,7 @@ import com.android.settings.wifi.calling.WifiCallingSettings;
 import com.android.settings.wifi.p2p.WifiP2pSettings;
 
 import org.carbonrom.carbonfibers.CarbonFibers;
+import org.carbonrom.carbonfibers.fragments.system.SmartPixels;
 
 public class SettingsGateway {
 
@@ -264,6 +265,7 @@ public class SettingsGateway {
             ToggleBackupSettingFragment.class.getName(),
             PreviouslyConnectedDeviceDashboardFragment.class.getName(),
             CarbonFibers.class.getName(),
+            SmartPixels.class.getName(),
     };
 
     public static final String[] SETTINGS_FOR_RESTRICTED = {
@@ -303,5 +305,6 @@ public class SettingsGateway {
             Settings.EnterprisePrivacySettingsActivity.class.getName(),
             Settings.MyDeviceInfoActivity.class.getName(),
             Settings.CarbonFibersActivity.class.getName(),
+            Settings.SmartPixelsActivity.class.getName(),
     };
 }

GZOSP-EXT/packages_apps_settings

src/com/android/settings/core/gateway/SettingsGateway.java
@@ -148,6 +148,7 @@ import com.android.settings.wifi.p2p.WifiP2pSettings;
 import com.android.settings.wifi.savedaccesspoints.SavedAccessPointsWifiSettings;
 import com.android.settings.wifi.tether.WifiTetherSettings;
 import org.tesla.teslacoil.TeslaCoil;
+import org.tesla.teslacoil.fragments.system.SmartPixels;
 
 public class SettingsGateway {
 
@@ -286,6 +287,7 @@ public class SettingsGateway {
             GlobalActionsPanelSettings.class.getName(),
             DarkModeSettingsFragment.class.getName(),
             TeslaCoil.class.getName(),
+            SmartPixels.class.getName(),
     };
 
     public static final String[] SETTINGS_FOR_RESTRICTED = {
@@ -326,5 +328,6 @@ public class SettingsGateway {
             UserBackupSettingsActivity.class.getName(),
             Settings.TeslaCoilActivity.class.getName(),
             Settings.TeslaCoilSystemActivity.class.getName(),
+            Settings.SmartPixelsActivity.class.getName(),
     };
 }