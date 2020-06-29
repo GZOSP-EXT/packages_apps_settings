BlissRoms/platform_packages_apps_Settings

Settings: Initial import for Pixel-specific feature Settings
Change-Id: If9a87439e03be388de0e5b9059dc93c77243062d
Signed-off-by: Jackeagle <jackeagle102@gmail.com>

Android.mk
@@ -22,6 +22,7 @@ LOCAL_USE_AAPT2 := true
 
 LOCAL_SRC_FILES := $(call all-java-files-under, src)
 LOCAL_SRC_FILES += $(call all-java-files-under, ../Blissify/src) 
+LOCAL_SRC_FILES += $(call all-java-files-under, ../../../external/google/SettingsGoogle/src)
 
 LOCAL_STATIC_ANDROID_LIBRARIES := \
     androidx-constraintlayout_constraintlayout \
@@ -56,7 +57,8 @@ LOCAL_STATIC_JAVA_LIBRARIES := \
     org.lineageos.platform.internal
 
 LOCAL_RESOURCE_DIR := $(LOCAL_PATH)/res \
-    packages/apps/Blissify/res
+    packages/apps/Blissify/res \
+    external/google/SettingsGoogle/res
 
 LOCAL_PROGUARD_FLAG_FILES := proguard.flags
 

GZOSP-EXT/packages_apps_settings

Android.mk
@@ -21,7 +21,8 @@ LOCAL_MODULE_TAGS := optional
 LOCAL_USE_AAPT2 := true
 
 LOCAL_SRC_FILES := $(call all-java-files-under, src)
-LOCAL_SRC_FILES += $(call all-java-files-under, ../TeslaCoil/src)
+LOCAL_SRC_FILES += $(call all-java-files-under, ../TeslaCoil/src) 
+LOCAL_SRC_FILES += $(call all-java-files-under, ../../../external/google/SettingsGoogle/src)
 
 LOCAL_STATIC_ANDROID_LIBRARIES := \
     androidx-constraintlayout_constraintlayout \
@@ -54,7 +55,8 @@ LOCAL_STATIC_JAVA_LIBRARIES := \
     zxing-core-1.7
 
 LOCAL_RESOURCE_DIR := $(LOCAL_PATH)/res \
-    packages/apps/TeslaCoil/res
+    packages/apps/TeslaCoil/res \
+    external/google/SettingsGoogle/res
 
 LOCAL_PROGUARD_FLAG_FILES := proguard.flags
 